37e12b01d1f1db135b3bca59683bbb2055ae85e2
Washington Rodrigues <washingtonjunio16@gmail.com>
2022-11-04 23:33:38 -0300

    feat: create model Profile

---
+++ packages/registration/spec/models/profile_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Profile, type: :model do
+  describe 'associations' do
+    it { should belong_to(:user) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:user) }
+  end
+end
