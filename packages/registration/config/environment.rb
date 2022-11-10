5c2e8734a461476c76284adfafe776cf0a828dc9
Washington Rodrigues <washingtonjunio16@gmail.com>
2022-11-09 21:56:07 -0300

    fix: rubocop

--- packages/registration/config/environment.rb
+++ packages/registration/config/environment.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 # This is an _environment variable_ that is used by some of the Rake tasks to determine
 # if our application is running locally in development, in a test environment, or in production
 ENV['RACK_ENV'] ||= 'development'
 
 # Require in Gems
 require 'bundler/setup'
-Bundler.require(:default, ENV['RACK_ENV'])
+Bundler.require(:default, ENV.fetch('RACK_ENV', nil))
