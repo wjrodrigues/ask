aa200d25a97ef4050c01f5dca336ec16f929d609
Washington Rodrigues <washingtonjunio16@gmail.com>
2022-11-10 22:00:39 -0300

    feat: add dotenv

--- packages/registration/config/boot.rb
+++ packages/registration/config/boot.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'dotenv/load'
+
 require_relative 'environment'
 require_relative 'application'
 
--- packages/registration/spec/spec_helper.rb
+++ packages/registration/spec/spec_helper.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'dotenv/load'
+
 ENV['RACK_ENV'] ||= 'test'
 
 require 'support/simplecov'
