b7f6775e4ea22c84e405b77dd8f5993def9b391e
Washington Rodrigues <washingtonjunio16@gmail.com>
2022-11-15 14:55:38 -0300

    feat: clean and add command

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,14 +1,29 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  test: {
+    watch: false,
+    include: ['**/*.spec.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    globals: true,
+    environment: 'jsdom',
+    reporters: ['verbose'],
+    coverage: {
+      reporter: ["text", "json", "html"],
+      all: true,
+      lines: 80,
+      functions: 80,
+      branches: 80,
+      statements: 80,
+    },
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
