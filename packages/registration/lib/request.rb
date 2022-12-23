448b71f69c931bef9cb983d85358013d4c694258
Washington Rodrigues <washingtonjunio16@gmail.com>
2022-12-23 16:52:41 -0300

    other: error request handling

--- packages/registration/lib/request.rb
+++ packages/registration/lib/request.rb
@@ -8,12 +8,22 @@ module Lib
 
     def self.execute(url, args = {})
       RestClient::Request.execute(url:, timeout: TIMEOUT, **args)
+    rescue ArgumentError, Errno::EADDRNOTAVAIL => e
+      raise StandardError, e.message
     rescue StandardError => e
-      raise Lib::HttpError, e
+      raise Lib::HttpError.new(response: e.response)
     end
 
     private_constant :TIMEOUT
   end
 
-  class HttpError < StandardError; end
+  class HttpError < StandardError
+    attr_reader :response
+
+    def initialize(response: nil)
+      @response = response
+
+      super
+    end
+  end
 end
--- packages/registration/spec/lib/request_spec.rb
+++ packages/registration/spec/lib/request_spec.rb
@@ -3,9 +3,9 @@
 require 'spec_helper'
 
 RSpec.describe Lib::Request, type: :lib do
-  describe '#get' do
+  describe '#execute' do
     context 'when request any URL' do
-      it 'returns HTTP::Response instance' do
+      it 'returns RestClient::Response instance' do
         VCR.use_cassette('request/get') do
           response = described_class.execute('https://dummyjson.com/products', method: :get)
 
@@ -13,8 +13,16 @@ RSpec.describe Lib::Request, type: :lib do
         end
       end
 
-      it 'raises any error' do
-        expect { described_class.execute('') }.to raise_error(Lib::HttpError)
+      it 'raises ClientRequest error' do
+        expect { described_class.execute('') }.to raise_error(StandardError)
+      end
+
+      it 'raises request error' do
+        VCR.use_cassette('request/get_error') do
+          expect do
+            described_class.execute('https://dummyjson.com/product', method: :get)
+          end.to raise_error(Lib::HttpError)
+        end
       end
     end
   end
