9eec35bf15cea685b32c2dec358bfc3cf156ab7a
Washington Rodrigues <washingtonjunio16@gmail.com>
2022-12-29 18:49:23 -0300

    feat: create Lib::Messager

--- packages/registration/lib/messenger.rb
+++ packages/registration/lib/messenger.rb
@@ -2,20 +2,5 @@
 
 module Lib
   class Messenger
-    TARGETS = { rabbitmq: 'Lib::Notification::Rabbitmq' }.freeze
-    GET_TARGET = lambda do |target|
-      [target] => [Symbol]
-
-      raise NotImplementedError, "#{target} unsupported" unless TARGETS.key?(target)
-
-      TARGETS.fetch(target)
-    end
-
-    # def self.create(password:, email:, target:, first_name: nil, last_name: nil)
-    #   klass = GET_TARGET.call(target).constantize.new
-    #   klass.create(first_name:, last_name:, password:, email:)
-    # end
-
-    private_constant :TARGETS, :GET_TARGET
   end
 end
