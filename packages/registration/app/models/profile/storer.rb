8883b342b044d5398f604e7df76899f78842ba58
Washington Rodrigues <washingtonjunio16@gmail.com>
2023-01-13 22:13:16 -0300

    other: change params call Profile::Storer

--- packages/registration/app/models/profile/storer.rb
+++ packages/registration/app/models/profile/storer.rb
@@ -5,35 +5,33 @@ module Profile
     ACTIONS = %i[presigned_url].freeze
     EXTENSIONS = %i[jpg jpeg png].freeze
 
-    attr_accessor :extension, :user, :storage, :action
+    attr_accessor :params, :storage
 
-    def initialize(extension:, user:, action:, storage: Lib::Storage)
-      self.extension = extension
-      self.user = user
+    def initialize(params, storage: Lib::Storage)
+      self.params = params
       self.storage = storage
-      self.action = action
 
       super
     end
 
     def call
-      if action.nil? || !ACTIONS.include?(action.to_sym)
+      if params.action.nil? || !ACTIONS.include?(params.action.to_sym)
         return response.add_error(:'models.profile.storer.errors.unsupported_action')
       end
 
-      response.add_result({ url: send(action.to_sym) })
+      response.add_result({ url: send(params.action.to_sym) })
     end
 
-    private :extension=, :user=, :storage=, :action=
+    private :params, :storage
     private_constant :ACTIONS, :EXTENSIONS
 
     def presigned_url
-      if extension.nil? || !EXTENSIONS.include?(extension.to_sym)
+      if params.extension.nil? || !EXTENSIONS.include?(params.extension.to_sym)
         return response.add_error(:'models.profile.storer.errors.unsupported_extension')
       end
 
       storage_instance = storage.new(folder_name: 'profilePictures')
-      file_name = "#{user.id}.#{extension}"
+      file_name = "#{params.user_id}.#{params.extension}"
 
       storage_instance.presigned_url(file_name)
     end
--- packages/registration/spec/models/profile/storer_spec.rb
+++ packages/registration/spec/models/profile/storer_spec.rb
@@ -4,11 +4,14 @@ require 'spec_helper'
 
 RSpec.describe ::Profile::Storer, type: :model do
   describe '#call' do
+    let(:user) { create(:user) }
+
     context 'when values are valid' do
       it 'returns response with presigned url' do
-        user = create(:user)
-
-        response = described_class.call(extension: 'jpg', user:, action: :presigned_url)
+        params = Controller::Request.call(
+          extension: 'jpg', user_id: user.id, action: :presigned_url
+        )
+        response = described_class.call(params)
 
         expect(response.ok?).to be_truthy
         expect(response.result[:url]).to start_with("https://s3.amazonaws.com/profilePictures/#{user.id}.jpg")
@@ -17,9 +20,8 @@ RSpec.describe ::Profile::Storer, type: :model do
 
     context 'when action are not valid' do
       it 'returns response with errors' do
-        user = create(:user)
-
-        response = described_class.call(extension: 'jpg', user:, action: :upload)
+        params = Controller::Request.call(extension: 'jpg', user_id: user.id, action: :upload)
+        response = described_class.call(params)
 
         expect(response.ok?).not_to be_truthy
         expect(response.errors).to eq(['Unsupported action'])
@@ -28,9 +30,10 @@ RSpec.describe ::Profile::Storer, type: :model do
 
     context 'when extension are not valid' do
       it 'returns response with errors' do
-        user = create(:user)
-
-        response = described_class.call(extension: 'mp4', user:, action: :presigned_url)
+        params = Controller::Request.call(
+          extension: 'mp4', user_id: user.id, action: :presigned_url
+        )
+        response = described_class.call(params)
 
         expect(response.ok?).not_to be_truthy
         expect(response.errors).to eq(['Unsupported extension'])
