3706ab337e827cec5b75f6ee24de0929f0165452
Washington Rodrigues <washingtonjunio16@gmail.com>
2023-02-15 20:34:25 -0300

    feat: create Middleware::Auth

--- packages/registration/app/middleware/auth.rb
+++ packages/registration/app/middleware/auth.rb
@@ -2,9 +2,29 @@
 
 module Middleware
   class Auth
-    def self.check!(app, _token)
+    EXTRACT_REQUEST = ->(app, attr) { app.request.env[attr] }
+
+    def self.check!(app)
+      token = EXTRACT_REQUEST.call(app, 'HTTP_AUTHORIZATION')
+
+      return if token.nil? || Lib::Auth.valid_token?(token)
 
       app.halt Controller::Response::REASONS[:UNAUTHORIZED]
     end
+
+    def self.current_user(app)
+      return if app.nil?
+
+      return app.session['current_user'] unless app.session['current_user'].nil?
+
+      auth_token = EXTRACT_REQUEST.call(app, 'HTTP_AUTHORIZATION')
+      token = Lib::Auth.decode_token(auth_token)
+
+      return unless token
+
+      app.session['current_user'] = User::Repository.find(value: token['email']).slice(:id, :email)
+    end
+
+    private_constant :EXTRACT_REQUEST
   end
 end
--- packages/registration/spec/middleware/auth_spec.rb
+++ packages/registration/spec/middleware/auth_spec.rb
@@ -3,11 +3,60 @@
 require 'spec_helper'
 
 RSpec.describe Middleware::Auth, type: :middleware do
-  describe '#check' do
-    context '--' do
-      it '-' do
-        binding.pry
-        Middleware::Auth.check!(double(halt: nil), '')
+  describe '#current_user' do
+    context 'when the session is not defined' do
+      it 'calls decode_token from Lib::Auth' do
+        token = 'ask_123'
+        env = double(env: { 'HTTP_AUTHORIZATION' => token })
+        app = double(request: env, session: {})
+        user = create(:user, email: 'billie_harvey@bauch-hills.info')
+
+        expect(Lib::Auth)
+          .to receive(:decode_token)
+          .with(token)
+          .and_return({ 'email' => user.email })
+
+        result = Middleware::Auth.current_user(app)
+
+        expect(result).to eq({ id: user.id, email: user.email })
+      end
+    end
+
+    context 'when the session is defined' do
+      it 'does not call decode_token from Lib::Auth' do
+        user = create(:user, email: 'billie_harvey@bauch-hills.info')
+        app = double(session: { 'current_user' => { id: user.id, email: user.email } })
+
+        expect(Lib::Auth).not_to receive(:decode_token)
+
+        result = Middleware::Auth.current_user(app)
+
+        expect(result).to eq({ id: user.id, email: user.email })
+      end
+    end
+  end
+
+  describe '#check!' do
+    context 'when the token is valid' do
+      it 'returns nil' do
+        env = double(env: { 'HTTP_AUTHORIZATION' => 'ask_123' })
+        app = double(request: env, session: {})
+
+        expect(Lib::Auth).to receive(:valid_token?).and_return(true)
+
+        expect(Middleware::Auth.check!(app)).to be_nil
+      end
+    end
+
+    context 'when the token is invalid' do
+      it 'returns http UNAUTHORIZED status' do
+        env = double(env: { 'HTTP_AUTHORIZATION' => 'ask_123' })
+        app = double(request: env, session: {})
+
+        expect(Lib::Auth).to receive(:valid_token?).and_return(false)
+        expect(app).to receive(:halt).with(401)
+
+        Middleware::Auth.check!(app)
       end
     end
   end
